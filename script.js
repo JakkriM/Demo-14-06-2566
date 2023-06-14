b15a680665027e47531239d1e19c302f307ae0e6
JakkriM <83633096+JakkriM@users.noreply.github.com>
2023-06-14 14:59:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+function getnumber(value) {
+  let chackcount = document.getElementById("display").value;
+  chackcount = parseFloat(chackcount.replaceAll(",", ""));
+  if (chackcount.toString().length < 10) {
+    let num = (document.getElementById("display").value += value);
+    num = parseFloat(num.replaceAll(",", ""));
+    document.getElementById("display").value = displayprocess(num);
+  }
+}
+
+function displayprocess(num) {
+  let value = num.toLocaleString("en-US");
+  return value;
+}
+
+function BtnReset() {
+  document.getElementById("display").value = "";
+}
+
+function Btndel(value) {
+  let num = (document.getElementById("display").value = value
+    .toString()
+    .slice(0, -1));
+  if (num != 0) {
+    num = parseFloat(num.replaceAll(",", ""));
+  }
+  document.getElementById("display").value = displayprocess(num);
+}
+
+let x;
+function calculate(value) {
+  let num = document.getElementById("display").value;
+  num = parseFloat(num.replaceAll(",", ""));
+  num += value;
+  x = num;
+  document.getElementById("display").value = "";
+}
+
+function equal() {
+  console.log(x);
+  let num = document.getElementById("display").value;
+  num = parseFloat(num.replaceAll(",", ""));
+  console.log(x, " and ", num);
+  x += num;
+  document.getElementById("display").value = displayprocess(eval(x));
+}
+
+function decimal() {
+  let num = (document.getElementById("display").value += ".");
+  num = parseFloat(num.replaceAll(",", ""));
+}
+
+let btnTheme = document.getElementById("switch");
+let slider = document.querySelector("#slider");
+let body = document.querySelector("body");
+
+let ThemeNum = 1;
+function switchTheme() {
+  if (ThemeNum + 1 > 3) {
+    ThemeNum = 1;
+  } else {
+    ThemeNum += 1;
+  }
+  switch (ThemeNum) {
+    case 1:
+      document.documentElement.className = "theme1";
+      slider.style.marginLeft = "5px";
+      break;
+
+    case 2:
+      document.documentElement.className = "theme2";
+      slider.style.marginLeft = "25px";
+      break;
+
+    case 3:
+      document.documentElement.className = "theme3";
+      slider.style.marginLeft = "45px";
+      break;
+
+    default:
+      document.documentElement.className = "theme1";
+      slider.style.marginLeft = "5px";
+  }
+}
